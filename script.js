3bae1068cce16d2ea0126f077c8f590c5bedcbbc
SaurabhMagar15 <saurabhmagar151771@gmail.com>
2024-09-11 10:42:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+const h2=document.querySelector("h2");
+const images=document.querySelectorAll("img");
+var p=document.querySelectorAll("p");
+let button=document.querySelector(".btn");
+let comp=0;
+let you=0;
+function newGame(comp,you){
+button.addEventListener('click',()=>{
+    p[2].innerText=0;
+    p[4].innerText=0;
+    h2.innerText="start";
+    
+})
+}
+newGame();
+
+function ani(){
+for(let i=0;i<3;i++){
+    images[i].addEventListener('mouseover',()=>{
+        images[i].classList.add("animations");
+    })
+    images[i].addEventListener('mouseleave',()=>{
+        images[i].classList.remove("animations");
+    })
+}
+}
+ani();
+
+
+
+
+
+function chooseSymbol()
+{   let computerInput=Math.floor(Math.random()*2);
+    for(let i=0;i<3;i++){
+        images[i].addEventListener("click",()=>{
+            var userInput=i;
+            chooseWinner(userInput,computerInput);
+            
+            
+        })
+
+    }
+    
+}
+chooseSymbol();
+
+function chooseWinner(userInput,computerInput){
+    let Winner="";
+    if(userInput==computerInput){
+        Winner="It's a Tie";
+    }
+    else if(userInput==1 && computerInput==2){
+        Winner="computer";
+    }
+    else if(userInput==0 && computerInput==1){
+        Winner="computer";
+    }
+    else if(userInput==0 && computerInput==2){
+        Winner="player";
+    }
+    else if(userInput==2 && computerInput==1){
+        Winner="player";
+    }
+    else if(userInput==1 && computerInput==0){
+        Winner="player";
+    }
+    else if(userInput==2 && computerInput==0){
+        Winner="computer";
+    }
+
+    if(p[2].innerText=="0" && p[4].innerText=="0"){
+        you=0;
+        comp=0;
+    }
+    
+            
+    if(Winner=="computer"){
+        comp++;
+        p[4].innerText=comp;
+
+        
+    }
+    else if(Winner=="player"){
+        you++;
+        p[2].innerText=you;
+
+        
+    }
+    Winner="Winner is "+ Winner;
+    h2.innerText=Winner;
+
+}
+
+
